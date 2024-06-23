52e026e7d02a5fd972184be476f1ed8444d83b16
lmbsumatra <122442812+lmbsumatra@users.noreply.github.com>
2024-06-23 17:29:54 +0800

    :: this is for nextjs config

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+// pages/_app.js or pages/_app.tsx
+import { SessionProvider } from "next-auth/react";
+import "../app/globals.css";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+}
